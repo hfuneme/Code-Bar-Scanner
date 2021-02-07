5f2a09c7155f95a6fb6d435bb0c63b06593f34da
hfuneme <hfuneme@bancodebogota.com.co>
2021-02-07 17:15:24 -0500

    build of native android and 3 funtions of test for send and receive parammaters

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import 'Code-bar-scanner';
+import { Plugins } from '@capacitor/core';
+
+const { CodeBarScanner } = Plugins;
 
 @Component({
   selector: 'app-folder',
@@ -13,6 +17,27 @@ export class FolderPage implements OnInit {
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+  }
+
+
+  executeBarCode() {
+    CodeBarScanner.echo({value: 'plugin con BarCodeHHFM.com', id: 1989}).then(res => {
+        console.log(res);
+      }).catch(err => {
+        console.error(err);
+      });
+      CodeBarScanner.suma({numero1: 2, numero2: 5}).then(res => {
+        console.log("------SUMA---------");
+        console.log(res);
+      }).catch(err => {
+        console.error(err);
+      });
+      CodeBarScanner.resta({numero1: 12, numero2: 6}).then(res => {
+        console.log("------RESTA---------");
+        console.log(res);
+      }).catch(err => {
+        console.error(err);
+      });
   }
 
 }
