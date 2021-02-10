963b70e672d54d293c31aeccbcbb7712a60831de
hfuneme <hfuneme@bancodebogota.com.co>
2021-02-10 10:56:17 -0500

    Funcion de cambio de color, texto editable  y tollbar sin back button ni grio de pantalla

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -9,16 +9,18 @@ import com.getcapacitor.Plugin;
 import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
     // Initializes the Bridge
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
       add(CodeBarScanner.class);
-
     }});
   }
+
 }
